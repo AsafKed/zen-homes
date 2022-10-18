c2460d494012f2863f5d1a58361fe6c44b41589d
Asaf Kedem <musashishi@pop-os.localdomain>
2022-10-18 16:56:09 +0200

    Init

---
+++ src/components/golden-ratio-welcome/GoldenRatioWelcome.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div id="wrapper" data-configuration="1" data-roundness="1">
+      <div class="shape"></div>
+      <div class="shape"></div>
+      <div class="shape"></div>
+      <div class="shape"></div>
+      <div class="shape"></div>
+      <div class="shape"></div>
+      <div class="shape"></div>
+    </div>
+    <div style="color:white">Configuration {{ combination.configuration }}</div>
+    <div style="color:white">Shape {{ combination.roundness }}</div>
+    </div>
+</template>
+
+<script src="./golden-ratio-welcome.js"></script>
+<style lang="scss" src="./golden-ratio-welcome.scss"></style>
